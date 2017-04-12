71e9bec0446542851ce674300d0f593c4e56792d
Mehedee Hassan <razin.meha@gmail.com>
2017-04-12 21:52:51 +0600

    add write to file

--- webscrapping/test2/writeFileToDisk.py
+++ webscrapping/test2/writeFileToDisk.py
@@ -2,4 +2,42 @@
 
 import  newspaper
 
-def writeFile(coneteHtml):
+def writeFile(coneteHtml,filename):
+    newsArticle = newspaper.Article()
+    newsArticle.set_html(coneteHtml)
+
+    newsArticle.parse()
+
+    text = newsArticle.publish_date
+    text = text+"\n"+newsArticle.title
+    text = text+"\n"+newsArticle.text
+
+
+    file = open(str(filename) ,'w')
+    file.write(text)
+    file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
